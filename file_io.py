084c326c6e1ecc8564328e5e3858abaecd42f33a
Roxana <roxana.te@web.de>
2016-10-10 09:30:17 +0300

    [saving arrays on disk]

--- file_io.py
+++ file_io.py
@@ -1,4 +1,8 @@
 Python 2.7.12 (default, Jul  1 2016, 15:12:24) 
 [GCC 5.4.0 20160609] on linux2
 Type "copyright", "credits" or "license()" for more information.
+>>> # NumPy is able to save and load data to and from disk either in text or in binary format.
+>>> 
+>>> # 1) Storing arrays on disk in binary format
+>>> import numpy as np
 >>> 
